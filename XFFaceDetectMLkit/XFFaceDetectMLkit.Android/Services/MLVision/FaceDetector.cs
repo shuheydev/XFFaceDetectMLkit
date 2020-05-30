fa8794fa305a463a3db462123450c39173ea2f10
Shuhey <shuhey.dev@gmail.com>
2020-05-30 17:44:42 +0900

    Add project files.

---
+++ XFFaceDetectMLkit/XFFaceDetectMLkit.Android/Services/MLVision/FaceDetector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase;
+using XFFaceDetectMLkit.Droid.Services.MLVision;
+using XFFaceDetectMLkit.Services.MLVision;
+
+[assembly:Xamarin.Forms.Dependency(typeof(FaceDetector))]
+namespace XFFaceDetectMLkit.Droid.Services.MLVision
+{
+    public class FaceDetector:IFaceDetector
+    {
+        public readonly FirebaseApp Firebase_App; 
+        public FaceDetector()
+        {
+            var options = new FirebaseOptions.Builder()
+                .SetApplicationId("なんでもいいよ")
+                .Build();
+            Firebase_App = FirebaseApp.InitializeApp(null, options);
+        }
+    }
+}
---
+++ XFFaceDetectMLkit/XFFaceDetectMLkit/Services/MLVision/IFaceDetector.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFFaceDetectMLkit.Services.MLVision
+{
+    public interface IFaceDetector
+    {
+    }
+}
